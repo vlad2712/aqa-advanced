d832734bf94f97ea23efd7427c0263c10d4e1028
Vladyslav Andrusyk <vlad.andrusik99@gmail.com>
2025-02-04 20:51:50 +0100

    Create 4 files with home tasks.

---
+++ jsBasics/task1.js
@@ -0,0 +1,17 @@
+// Завдання 1: Оголошення змінних для примітивних типів
+
+// Створіть програму, в якій ви оголосите змінні для кожного з примітивних типів даних: рядок, число,
+// буль, null та undefined. Присвойте їм значення та виведіть кожну змінну на консоль.
+
+
+let stringVariable = `Hello, it's string`;
+let numberVariable = 27;
+let boolVariable = true;
+let nullVariable = null;
+let undefinedVariable = undefined;
+
+console.log(`stringVariable: ${stringVariable}`);
+console.log(`numberVariable: ${numberVariable}`);
+console.log(`boolVariable: ${boolVariable}`);
+console.log(`nullVariable: ${nullVariable}`);
+console.log(`undefinedVariable: ${undefinedVariable}`);
