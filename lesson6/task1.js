13226ead2c9235aab077815e824939c848f7259a
Vladyslav Andrusyk <vlad.andrusik99@gmail.com>
2025-02-13 13:36:29 +0100

    Home task 6 - done

---
+++ lesson6/task1.js
@@ -0,0 +1,16 @@
+
+const squareArea1 = (width, height) => {
+    return width * height
+    }
+
+console.log(squareArea1(5, 10))
+// ----------------------------------------------------------
+function squareArea2(width, height) {
+    return width * height
+    }
+
+console.log(squareArea2(5, 10))
+// ----------------------------------------------------------
+const squareArea3 = (width, height) =>  width * height
+
+console.log(squareArea3(5, 10))
